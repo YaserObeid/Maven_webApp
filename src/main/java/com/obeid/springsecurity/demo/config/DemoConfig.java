09ddf18b18ea233d694a56a0c9bada9f20a3f9ce
YaserObeid <yaser.obeid@hotmail.com>
2022-02-07 18:11:08 +0100

    inject the properties

--- src/main/java/com/obeid/springsecurity/demo/config/DemoConfig.java
+++ src/main/java/com/obeid/springsecurity/demo/config/DemoConfig.java
@@ -1,9 +1,11 @@
 package com.obeid.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -13,8 +15,12 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.obeid.springsecurity.demo")
-@PropertySource("classpath:test.properties")
+@PropertySource("classpath:db.properties")
 public class DemoConfig implements WebMvcConfigurer{
+	
+	// inject the db.properties
+	@Autowired
+	private Environment env;
 	
 	// define Internal Resource ViewResolver
 	
@@ -39,6 +45,8 @@ public class DemoConfig implements WebMvcConfigurer{
 		
 		registry.addResourceHandler("/asset/**").addResourceLocations("/asset/");
 	}
+	
+	
 	
 	
 	
--- src/main/webapp/WEB-INF/view/test.jsp
+++ src/main/webapp/WEB-INF/view/test.jsp
@@ -4,20 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Test</title>
     <!--local css-->
     <link rel="stylesheet" type="text/css" href="asset/css/main.css">
-    <link rel="stylesheet" type="text/css" href="css/demo.css">
 </head>
 <body>
     <h1 id="demo">Home Page</h1>
 
     <button onclick="changeText()">Change the text above</button>
-    
-    <h2>${test}</h2>
+   
 
 
-    <!--local javascript-->
-    <script src="asset/js/main.js"></script>
 </body>
 </html>
