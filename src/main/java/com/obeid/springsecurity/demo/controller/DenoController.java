08a4419542d510d853223651fa3394f38b40983f
YaserObeid <yaser.obeid@hotmail.com>
2022-02-07 12:46:50 +0100

    controller for home

--- src/main/java/com/obeid/springsecurity/demo/controller/DenoController.java
+++ src/main/java/com/obeid/springsecurity/demo/controller/DenoController.java
@@ -1,5 +1,15 @@
 package com.obeid.springsecurity.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DenoController {
+	
+	@GetMapping("/")
+	public String showHome() {
+		
+		return "home";
+	}
 
 }
